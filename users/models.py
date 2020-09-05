91a2763f82f09d197442257bdbc990e308d6d417
smilejakdu <ash982416@gmail.com>
2020-09-06 00:28:51 +0900

    user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username   = models.Model(max_length = 200 , null = True)
+    email      = models.Model(max_length=200 , null = True , unique=True)
+    password   = models.Model(max_length=200 , null = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+import json
+import jwt
+import re
+import bcrypt
+import requests
 
-# Create your views here.
+from django.views           import View
+from django.http            import HttpResponse, JsonResponse
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from todo_app.my_settings   import SECRET_KEY,   ALGORITHM
+
+
+class SignUpView(View):
+    def post(self , request):
+        data = json.loads(request.body)
+
+        try:
+            for d in data:
+                if not data[d]:
+                    return JsonResponse({"message":f"doesnot_{d}"} , status = 400)
+
+            validate_email(data['email'])
+
+            if len(data['password']) < 5:
+                return JsonResponse({"message":"SHORT_PASSWORD"} , status = 400)
+
+            User(
+                email    = data['email'],
+                username = data['username'],
+                password = data['password'],
+            ).save()
+
+            return HttpResponse(status = 200)
+
+
+        except KeyError:
+            return JsonResponse({"message" : "INVALID_KEY"},status = 400)
+
+        except ValidationError:
+            return HttpResponse(status=400)
+
+        except Exception as e:
+            return JsonResponse({"message" : e},status = 400)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            validate_email(data['email'])
+
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
+
+                if bcrypt.checkpw(data['password'].encode('utf-8'),
+                                  user.password.encode('utf-8')):
+
+                    token = jwt.encode({'email': data['email']},
+                                           SECRET_KEY['secret'],
+                                           algorithm=ALGORITHM).decode()
+
+                    return JsonResponse({'access': token}, status=200, content_type="application/json")
+
+                return HttpResponse(status=401)
+
+            return HttpResponse(status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=400)
+
+    def get(self , request):
+        data = list(User.
+                    objects.
+                    values("email"))
+
+        return JsonResponse({"data" : list(data)}, status = 200)
