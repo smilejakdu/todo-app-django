2ae78b1e8c7be1b38debe0c7d6bd6e56215ea2ac
smilejakdu <ash982416@gmail.com>
2020-09-08 02:14:28 +0900

    todo_list_back

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,9 @@ from users.models import User
 class Todo(models.Model):
     title       = models.CharField(max_length=200 , null=True)
     description = models.TextField()
-    user = models.ForeignKey("users.User" , on_delete=models.CASCADE)
+    user        = models.ForeignKey("users.User" , on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add = True)
+    updated_at  = models.DateTimeField(auto_now = True)
 
     class Meta:
         db_table="todos"
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 import json
-from .models         import Review
+from .models         import Todo
 from products.models import Product
 from account.models  import User
 from account.utils   import login_check
@@ -8,12 +8,38 @@ from django.http     import HttpResponse, JsonResponse
 
 class TodoView(View):
     @login_check
-    def post(self ,request):
-        return
+    def post(self ,request , user_id):
+        data = json.loads(request.body)
 
-    @login_check
+        try:
+            for d in data:
+                if not data[d]:
+                    return JsonResponse({"message" : f"doesnot_{d}"} , status = 400)
+
+            Todo(
+                title       = data['title'],
+                description = data['description'],
+                user_id     = user_id
+            ).save()
+
+            return HttpResponse(status = 200)
+
+        except KeyError:
+            return JsonResponse({"message" : "invalid_key"} , status = 400)
+
+        except Exception as e:
+            return JsonResponse({"message" : e} , status = 400)
+
     def get(self , request):
-        return
+
+        try:
+            data = (Todo.
+                    objects.
+                    values())
+            return JsonResponse({"data" : list(data)} , status = 200)
+
+        except Exception as e:
+            return JsonResponse({"message" : e} , status = 400)
 
 class TodoDetailView(View):
     @login_check
